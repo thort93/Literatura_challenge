9426ad6c28854c23847bf8e702848ef0ff6e281f
thort93 <119140146+thort93@users.noreply.github.com>
2025-08-16 16:56:29 -0500

    Add files via upload

---
+++ src/test/java/com/alura/hector/literaturaChallenge/LiteraturaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.hector.literaturaChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraturaChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
